95de427ec25558935fd9a5b107993dec637e499e
H0stk3rn3l <6146095+H0stk3rn3l@users.noreply.github.com>
2020-04-03 18:34:57 +0200

    Update configure.sh

--- Scripts/configure.sh
+++ Scripts/configure.sh
@@ -1,5 +1,11 @@
 #!/bin/bash
 
+function datef() {
+    # Output:
+    # Sat Jun  8 20:29:08 2019
+    date "+%a %b %-d %T %Y"
+}
+
 if [ ! -c /dev/net/tun ]; then
     echo "$(datef) Creating tun/tap device."
     mknod /dev/net/tun c 10 200
