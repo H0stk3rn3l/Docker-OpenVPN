3ce165393ff89ddc1a8f332903796f9225d999d6
H0stk3rn3l <6146095+H0stk3rn3l@users.noreply.github.com>
2020-04-04 22:35:51 +0200

    Update configure.sh

--- Scripts/configure.sh
+++ Scripts/configure.sh
@@ -1,14 +1,23 @@
 #!/bin/bash
 
-function datef() {
-    # Output:
-    # Sat Jun  8 20:29:08 2019
-    date "+%a %b %-d %T %Y"
-}
-
 if [ ! -c /dev/net/tun ]; then
     mkdir -p /dev/net
     mknod /dev/net/tun c 10 200
     chmod 600 /dev/net/tun
     cat /dev/net/tun
 fi
+
+# Allow UDP traffic on port 1194.
+iptables -A INPUT -i eth0 -p udp -m state --state NEW,ESTABLISHED --dport 1194 -j ACCEPT
+iptables -A OUTPUT -o eth0 -p udp -m state --state ESTABLISHED --sport 1194 -j ACCEPT
+
+# Allow traffic on the TUN interface.
+iptables -A INPUT -i tun0 -j ACCEPT
+iptables -A FORWARD -i tun0 -j ACCEPT
+iptables -A OUTPUT -o tun0 -j ACCEPT
+
+# Allow forwarding traffic only from the VPN.
+iptables -A FORWARD -i tun0 -o eth0 -s 10.8.0.0/24 -j ACCEPT
+iptables -A FORWARD -m state --state ESTABLISHED,RELATED -j ACCEPT
+
+iptables -t nat -A POSTROUTING -s 10.8.0.0/24 -o eth0 -j MASQUERADE
